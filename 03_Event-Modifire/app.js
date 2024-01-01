148691b12c85cb9437de585be91e843620530334
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-01 15:59:17 +0600

    Event Modifier work

--- 03_Event-Modifire/app.js
+++ 03_Event-Modifire/app.js
@@ -2,12 +2,22 @@
 const app = Vue.createApp({
     data(){
         return{
-           
+           name: "",
         }
     },
     methods: {
         handleKeyup(e){
+            // console.log(e.target.value);
+            this.name = e.target.value;
+        },
+        handleSubmitHere(e){
             console.log(e)
+        },
+        handleViewClick(){
+            console.log("View Event")
+        },
+        handleClick(){
+            console.log("Click Event");
         }
     }
 });
