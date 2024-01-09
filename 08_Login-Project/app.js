ba21c73ee00578dc2d536e31bec27952ec14efd5
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-09 23:08:21 +0600

    Working Yet perfectly

--- 08_Login-Project/app.js
+++ 08_Login-Project/app.js
@@ -13,22 +13,44 @@ app.component("login-form", {
     template: `
         <form @submit.prevent="handleSubmit">
             <h1>{{ title }}</h1>
-            <input type="email" v-model="labelEmail" />
-            <input type="password" v-model="labelPassword" />
+            <custom-input type="email" v-model="email" v-bind:label="labelEmail" />
+            <custom-input type="password" v-model="password" v-bind:label="labelPassword" />
 
             <button>Login</button>
         </form>
     `,
+    components: ['custom-input'],
     data() {
         return {
             title: "Login Form",
-            labelEmail: "",
-            labelPassword: "",
+            email: "",
+            password: "",
+            labelEmail: "Email",
+            labelPassword: "Password"
         }
     },
     methods: {
         handleSubmit() {
-            console.log(this.labelEmail + " " + this.labelPassword)
+            console.log(this.email + " " + this.password)
+        }
+    }
+});
+
+app.component("custom-input", {
+    template: `
+        <label>{{ label }}
+            <input type="text" v-model="inputValue" />
+        </label>
+    `,
+    props: ["label", "modelValue"],
+    computed: {
+        inputValue: {
+            get() {
+                return this.modelValue;
+            },
+            set(value) {
+                console.log(value)
+            }
         }
     }
 });
