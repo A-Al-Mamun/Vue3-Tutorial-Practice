00071e50da508d20591507635fa6a8d0bfbe1fe2
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-14 23:24:01 +0600

    Try to build the Game

--- 07_TicTacToyGame/app.js
+++ 07_TicTacToyGame/app.js
@@ -3,16 +3,33 @@ let app = Vue.createApp({
         return{
             boxValue: "",
             turn: true,
+            checkTurm: [
+                [0,1,2],
+                [0,4,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [2,4,6],
+                [3,4,5],
+                [6,7,8],
+            ]
         }
     },
     methods: {
         setValue(){
-            if(this.turn){
-                this.boxValue = "X";
-                this.turn = false;
-            }else{
-                this.boxValue = 'O';
-            }
+            let boxes = document.querySelectorAll('.box');
+            boxes.forEach(item => {
+                item.addEventListener('click', () => {
+                    if(this.turn){
+                        this.boxValue = "X";
+                        this.turn = false;
+                    }else{
+                        this.boxValue = 'O';
+                    }
+                })
+            })
+
+            
         }
     }
 });
