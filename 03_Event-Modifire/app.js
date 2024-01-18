3307e1e7b63c3864727bd6ff30356ad0d532933b
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-18 23:27:15 +0600

    event modifier practice

--- 03_Event-Modifire/app.js
+++ 03_Event-Modifire/app.js
@@ -3,12 +3,14 @@ const app = Vue.createApp({
     data(){
         return{
            name: "",
+        //    keyDownValue: ""
         }
     },
     methods: {
         handleKeyup(e){
             // console.log(e.target.value);
             this.name = e.target.value;
+            // this.keyDownValue = e.key;
         },
         handleSubmitHere(e){
             console.log(e)
