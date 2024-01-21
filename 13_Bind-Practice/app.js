0739f4c2e3b0941d42e40eddbda1324bbb3467d5
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-21 19:44:45 +0600

    Computed properties practice in 13 folder bind-practice

--- 13_Bind-Practice/app.js
+++ 13_Bind-Practice/app.js
@@ -1,29 +1,48 @@
 let app = Vue.createApp({
     data(){
         return{
-            name: null,
-            email: null,
-            phone: null,
-            gender: null,
+            inputName: null,
+            inputEmail: null,
+            inputPhone: null,
+            inputGender: null,
+            important: false,
             infoStore: [
                 {
                     name: "Abdullah Al Mamun",
                     email: "abdullahalmamun3073@gmail.com",
                     phone: "01309068352",
                     gender: "Male",
-                },
-                {
+                    important: false,
+
                     lname: "Name :",
                     lemail: "Email :",
                     lphone: "Phone :",
                     lgender: "Gender :",
+                    limportant: "Important ?:"
                 }
             ]
         }
     },
     methods: {
         showInfo(){
-
+            let item = {
+                name: this.inputName,
+                email: this.inputEmail,
+                gender: this.inputGender
+            };
+            this.infoStore.push(item);
+            this.inputName = null;
+            this.inputEmail = null;
+            this.inputGender = null;
+        }
+    },
+    computed: {
+        check(){
+            if(this.important){
+                return "Yes"
+            }else{
+                return "No Need"
+            }
         }
     }
 });
